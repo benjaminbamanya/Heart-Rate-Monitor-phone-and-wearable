73c0beae9ceffc9ce026f7c1fb351f799774dddf
benjaminbamanya <benjaminbamanya@gmail.com>
2019-01-29 15:15:34 +0100

    Simulated Readings

---
+++ smartwatch/src/main/java/healthmonitor/ehealth/faithworks/smartwatch/HeartbeatService.java
@@ -0,0 +1,174 @@
+package healthmonitor.ehealth.faithworks.smartwatch;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by uwe on 01.04.15.
+ */
+public class HeartbeatService extends Service implements SensorEventListener {
+
+    private static final String LOG_TAG = "MyHeart";
+    final Handler handler = new Handler();
+    private SensorManager mSensorManager;
+    private int currentValue = 0;
+    private IBinder binder = new HeartbeatServiceBinder();
+    private OnChangeListener onChangeListener;
+    Runnable runnable = new Runnable() {
+
+        @Override
+        public void run() {
+            try {
+                //do your code here
+                //Toast.makeText(HomeActivity.this, "Regular Click", Toast.LENGTH_SHORT).show();
+
+
+                int bpm = bpm();
+
+
+                onChangeListener.onValueChanged(bpm);
+
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+                //also call the same runnable to call it at regular interval
+                handler.postDelayed(this, 5000);
+            }
+        }
+    };
+    private GoogleApiClient mGoogleApiClient;
+
+    public int bpm() {
+        Random r = new Random();
+        int low = 10;
+        int high = 120;
+        int result = r.nextInt(high - low) + low;
+        return result;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // register us as a sensor listener
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        // delay SENSOR_DELAY_UI is sufficiant
+        boolean res = mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_UI);
+        Log.d(LOG_TAG, " sensor registered: " + (res ? "yes" : "no"));
+
+        if (!res) {
+
+            handler.post(runnable);
+
+        }
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Wearable.API).build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
+        Log.d(LOG_TAG, " sensor unregistered");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // is this a heartbeat event and does it have data?
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE && sensorEvent.values.length > 0) {
+            int newValue = Math.round(sensorEvent.values[0]);
+            //Log.d(LOG_TAG,sensorEvent.sensor.getName() + " changed to: " + newValue);
+            // only do something if the value differs from the value before and the value is not 0.
+            if (currentValue != newValue && newValue != 0) {
+                // save the new value
+                currentValue = newValue;
+                // send the value to the listener
+                if (onChangeListener != null) {
+                    Log.d(LOG_TAG, "sending new value to listener: " + newValue);
+                    onChangeListener.onValueChanged(newValue);
+                    sendMessageToHandheld(Integer.toString(newValue));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    /**
+     * sends a string message to the connected handheld using the google api client (if available)
+     *
+     * @param message
+     */
+    private void sendMessageToHandheld(final String message) {
+
+        if (mGoogleApiClient == null)
+            return;
+
+        Log.d(LOG_TAG, "sending a message to handheld: " + message);
+
+        // use the api client to send the heartbeat value to our handheld
+        final PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(mGoogleApiClient);
+        nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(NodeApi.GetConnectedNodesResult result) {
+                final List<Node> nodes = result.getNodes();
+                if (nodes != null) {
+                    for (int i = 0; i < nodes.size(); i++) {
+                        final Node node = nodes.get(i);
+                        Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), message, null);
+                    }
+                }
+            }
+        });
+
+    }
+
+
+    // interface to pass a heartbeat value to the implementing class
+    public interface OnChangeListener {
+        void onValueChanged(int newValue);
+    }
+
+    /**
+     * Binder for this service. The binding activity passes a listener we send the heartbeat to.
+     */
+    public class HeartbeatServiceBinder extends Binder {
+        public void setChangeListener(OnChangeListener listener) {
+            onChangeListener = listener;
+            // return currently known value
+            listener.onValueChanged(currentValue);
+        }
+
+    }
+
+
+}
---
+++ smartwatch/src/main/java/healthmonitor/ehealth/faithworks/smartwatch/WearActivity.java
@@ -0,0 +1,60 @@
+package healthmonitor.ehealth.faithworks.smartwatch;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+public class WearActivity extends Activity implements HeartbeatService.OnChangeListener {
+
+    private static final String LOG_TAG = "MyHeart";
+    private TextView mTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wear);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        // inflate layout depending on watch type (round or square)
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                // as soon as layout is there...
+                mTextView = (TextView) stub.findViewById(R.id.heartbeat);
+                // bind to our service.
+                bindService(new Intent(WearActivity.this, HeartbeatService.class), new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName componentName, IBinder binder) {
+                        Log.d(LOG_TAG, "connected to service.");
+                        // set our change listener to get change events
+                        ((HeartbeatService.HeartbeatServiceBinder) binder).setChangeListener(WearActivity.this);
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName componentName) {
+
+                    }
+                }, Service.BIND_AUTO_CREATE);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onValueChanged(int newValue) {
+        // will be called by the service whenever the heartbeat value changes.
+        mTextView.setText(Integer.toString(newValue));
+    }
+}
